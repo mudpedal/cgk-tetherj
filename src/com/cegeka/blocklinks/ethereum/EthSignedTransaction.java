0c9b466cb11c5586e434dbf563426fbeb9121c60
Andrei Grigoriu <mudpedal@vertexarmy.org>
2016-02-17 17:32:07 +0200

    EthSignedTransaction immutable way to store a finalised transaction

--- src/com/cegeka/blocklinks/ethereum/EthSignedTransaction.java
+++ src/com/cegeka/blocklinks/ethereum/EthSignedTransaction.java
@@ -2,33 +2,64 @@ package com.cegeka.blocklinks.ethereum;
 
 import java.math.BigInteger;
 
+import org.ethereum.crypto.SHA3Helper;
+
+/**
+ * Immutable Holds transaction data
+ * 
+ * @author andreicg
+ *
+ */
 public class EthSignedTransaction {
 
-	private final EthTransaction transaction;
-	private final String addressFrom;
+	private final String hash;
+	private final String from;
+	private final String to;
+	private final BigInteger value;
 	private final BigInteger nonce;
-	private final byte[] signature;
-	
-	public EthSignedTransaction(EthTransaction transaction, String addressFrom, BigInteger nonce, byte[] signature) {
-		this.transaction = transaction;
-		this.addressFrom = addressFrom;
+	private final byte[] signedEcodedData;
+
+	public EthSignedTransaction(String transactionHash, String from, String to, BigInteger value, BigInteger nonce,
+			byte[] signedEcodedData) {
+
+		this.hash = transactionHash;
+		this.from = from;
+		this.to = to;
+		this.value = value;
 		this.nonce = nonce;
-		this.signature = signature;
-	}
-	
-	public EthTransaction getTransaction() {
-		return transaction;
+		this.signedEcodedData = signedEcodedData;
 	}
 
-	public String getAddressFrom() {
-		return addressFrom;
+	public EthSignedTransaction(EthTransaction transaction, String from, BigInteger nonce, byte[] signedEcodedData) {
+		this.hash = SHA3Helper.sha3String(signedEcodedData);
+		this.from = from;
+		this.nonce = nonce;
+		this.value = transaction.getWeiValue();
+		this.to = transaction.getTo();
+		this.signedEcodedData = signedEcodedData;
+	}
+
+	public String getTransactionHash() {
+		return hash;
+	}
+
+	public String getFrom() {
+		return from;
 	}
 
 	public BigInteger getNonce() {
 		return nonce;
 	}
 
-	public byte[] getSignature() {
-		return signature;
+	public byte[] getSignedEcodedData() {
+		return signedEcodedData;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public BigInteger getValue() {
+		return value;
 	}
 }
