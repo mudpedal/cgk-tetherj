5fe60579dfde08ec90192401fdeb605ebd098658
alexbaloc <alexandru.baloc@cegeka.com>
2016-03-25 17:01:39 +0200

    move Utils library to Platform project

--- src/main/java/org/ethereum/util/Utils.java
+++ src/main/java/org/ethereum/util/Utils.java
@@ -1,155 +1,24 @@
 package org.ethereum.util;
 
-import org.ethereum.crypto.HashUtil;
-import org.spongycastle.util.encoders.DecoderException;
-import org.spongycastle.util.encoders.Hex;
-
 import java.lang.reflect.Array;
-import java.math.BigInteger;
 
 import java.net.URL;
 
 import java.security.SecureRandom;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-
 import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.regex.Pattern;
 
 import javax.swing.*;
 
 public class Utils {
 
     private static SecureRandom random = new SecureRandom();
-
-    /**
-     * @param number should be in form '0x34fabd34....'
-     * @return String
-     */
-    public static BigInteger unifiedNumericToBigInteger(String number) {
-
-        boolean match = Pattern.matches("0[xX][0-9a-fA-F]+", number);
-        if (!match)
-            return (new BigInteger(number));
-        else{
-            number = number.substring(2);
-            number = number.length() % 2 != 0 ? "0".concat(number) : number;
-            byte[] numberBytes = Hex.decode(number);
-            return (new BigInteger(1, numberBytes));
-        }
-    }
-
-    /**
-     * Return the Date object from an Unix's time() input in seconds
-     *
-     * @param timestamp seconds since start of Unix-time
-     * @return Date object
-     */
-    public static Date longToDateTime(long timestamp) {
-        return new Date(timestamp * 1000);
-    }
-
-    /**
-     * Return the Unix's time() in seconds from a date
-     *
-     * @param date input date object
-     * @return seconds since start of Unix-time
-     */
-    public static long dateTimetoLong(Date date) {
-        return (long) date.getTime()/1000;
-    }    
-    
-    /**
-     * Return formatted Date String: yyyy.MM.dd HH:mm:ss
-     * Based on Unix's time() input in seconds
-     *
-     * @param timestamp seconds since start of Unix-time
-     * @return String formatted as - yyyy.MM.dd HH:mm:ss
-     */
-    public static String longToDateTimeString(long timestamp) {
-        Date date = longToDateTime(timestamp);
-        DateFormat formatter = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
-        return formatter.format(date);
-    }
-    
     
     public static ImageIcon getImageIcon(String resource) {
         URL imageURL = ClassLoader.getSystemResource(resource);
         ImageIcon image = new ImageIcon(imageURL);
         return image;
     }
-
-    static BigInteger _1000_ = new BigInteger("1000");
-
-    public static String getValueShortString(BigInteger number) {
-        BigInteger result = number;
-        int pow = 0;
-        while (result.compareTo(_1000_) == 1 || result.compareTo(_1000_) == 0) {
-            result = result.divide(_1000_);
-            pow += 3;
-        }
-        return result.toString() + "·(" + "10^" + pow + ")";
-    }
-
-    /**
-     * Decodes a hex string to address bytes and checks validity
-     *
-     * @param hex - a hex string of the address, e.g., 6c386a4b26f73c802f34673f7248bb118f97424a
-     * @return - decode and validated address byte[]
-     */
-    public static byte[] addressStringToBytes(String hex) {
-        final byte[] addr;
-        try {
-            addr = Hex.decode(hex);
-        } catch (DecoderException addressIsNotValid) {
-            return null;
-        }
-
-        if (isValidAddress(addr))
-            return addr;
-        return null;
-    }
-
-    public static boolean isValidAddress(byte[] addr) {
-        return addr != null && addr.length == 20;
-    }
-
-    public static boolean isValidAddress(String addr) {
-        return addr != null && addr.length() == 40;
-    }
-    
-    
-    /**
-     * @param addr length should be 20
-     * @return short string represent 1f21c...
-     */
-    public static String getAddressShortString(byte[] addr) {
-
-        if (!isValidAddress(addr)) throw new Error("not an address");
-
-        String addrShort = Hex.toHexString(addr, 0, 3);
-
-        StringBuffer sb = new StringBuffer();
-        sb.append(addrShort);
-        sb.append("...");
-
-        return sb.toString();
-    }
-
-    /**
-     * @param addr address as encoded by Ethereum
-     * @return short string represent 1f21c...
-     */
-    public static String getAddressShortString(BigInteger ethereumAddress) {
-    	String address =  ethereumAddress.toString(16);
-    	if (!isValidAddress(address)) throw new Error("not an address");
-    	
-    	return "0x" + address;    	
-    }
-    
     
     public static SecureRandom getRandom() {
         return random;
@@ -169,24 +38,7 @@ public class Utils {
         }
         return Double.parseDouble(version.substring(0, pos - 1));
     }
-
-    public static String getHashListShort(List<byte[]> blockHashes) {
-        if (blockHashes.isEmpty()) return "[]";
-
-        StringBuilder sb = new StringBuilder();
-        String firstHash = Hex.toHexString(blockHashes.get(0));
-        String lastHash = Hex.toHexString(blockHashes.get(blockHashes.size() - 1));
-        return sb.append(" ").append(firstHash).append("...").append(lastHash).toString();
-    }
-
-    public static String getNodeIdShort(String nodeId) {
-        return nodeId == null ? "<null>" : nodeId.substring(0, 8);
-    }
-
-    public static long toUnixTime(long javaTime) {
-        return javaTime / 1000;
-    }
-
+    
     @SuppressWarnings("unchecked")
 	public static <T> T[] mergeArrays(T[] ... arr) {
         int size = 0;
@@ -218,13 +70,5 @@ public class Utils {
             for (int i = 0; i < n; i++) ret.append(s);
             return ret.toString();
         }
-    }
-    
-    public static byte[] addressStringToEthereumType(String hexAddress) {
-    	byte[] decodedAddress = addressStringToBytes(hexAddress);
-    	if (decodedAddress == null)
-    		return null;
-    	
-    	return HashUtil.sha3omit12(Arrays.copyOfRange(decodedAddress, 1, decodedAddress.length));
     }
 }
