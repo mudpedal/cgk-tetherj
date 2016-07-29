7b18a914f17d479bf5848bd86249228480a983bb
Andrei Grigoriu <mudpedal@vertexarmy.org>
2016-07-29 17:28:02 +0300

    Unlock wallet on non storage wallets now returns true everytime and ignores state

--- src/main/java/com/cegeka/tetherj/EthWallet.java
+++ src/main/java/com/cegeka/tetherj/EthWallet.java
@@ -125,13 +125,20 @@ public class EthWallet implements Serializable {
      * @return Returns true if succeeded.
      */
     public boolean unlock(String passphrase) {
-        privateKey = storage.getPrivateKey(passphrase);
+        if (storage.getCrypto() != null) {
+            privateKey = storage.getPrivateKey(passphrase);
+            if (privateKey == null) {
+                logger.debug("Failed to unlock wallet " + storage.toString());
+                return false;
+            }
+
+            logger.debug("Unlocked wallet " + storage.toString());
+        }
+
         if (privateKey == null) {
-            logger.debug("Failed to unlock wallet " + storage.toString());
             return false;
         }
 
-        logger.debug("Unlocked wallet " + storage.toString());
         return true;
     }
 
